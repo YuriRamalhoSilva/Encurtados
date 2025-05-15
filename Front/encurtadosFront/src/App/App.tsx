8b9bf1fc171ef6cd2a155da7fcbada7f831ed006
YuriRamalhoSilva <yuriramalhosilva67@gmail.com>
2025-05-15 20:11:34 -0300

    First commit

---
+++ Front/encurtadosFront/src/App/App.tsx
@@ -0,0 +1,17 @@
+import { Form } from '../Form/Form';
+import './App.css'
+import './Form.css'
+
+function App () {
+    return (
+        <div>
+
+            <h1>Encurtador de Links</h1>
+            <p>Cole seu link abaixo para obter uma versão curta</p>
+
+            <Form />
+        </div>
+    )
+}
+
+export default App
---
+++ Front/encurtadosFront/src/Form/Form.tsx
@@ -0,0 +1,11 @@
+
+
+export function Form () {
+    return (
+        <form action="">
+            <input type="text" placeholder="Cole seu link" />
+            <button type="submit">Enviar</button>
+        </form>
+    )
+
+}
